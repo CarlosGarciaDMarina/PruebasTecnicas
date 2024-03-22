1a323e6b166567cd244d7ae9569dbcef0bce4d34
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-03-22 20:56:25 +0100

    Correccion de archivos v2

--- 1-Ejercicio/Superheroes.js
+++ 1-Ejercicio/Superheroes.js
@@ -1,12 +1,4 @@
 /*
-Advertencias:
-- En español por fines didacticos, en la vida real usa nombres en ingles.
-- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
-- Ejercicios genéricos, puedes usar cualquier lenguaje.
-- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
-- Siempre mostrar el resultado en la consola / terminal.
-- Hay muchas soluciones validas para un mismo ejercicio.
- 
 Enunciado Ejercicio 1:
 Dado un array o un objeto de superhéroes de Marvel, haz un programa que pueda
 mostrar la información de un superheroe.
