7c6da9a74c22b3402388e4664b53b8e2326785a6
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-03-28 11:52:52 +0100

    Juego del ahorcado casi completo

--- 4-Ejercicio/ejercicio4.js
+++ 4-Ejercicio/ejercicio4.js
@@ -18,36 +18,58 @@ Te quedan 5 intentos
  
 */
 
-function juegoDelAhorcado(palabra) {  
+function juegoDelAhorcado(palabraSecreta) {  
+    // Crear variable palabra oculta con guiones bajos
+    let palabraOculta ="_".repeat(palabraSecreta.length);
+    // Variable con el numero de intentos
+    let intentos = 5;
 
+    // Bucle para pedir letras, si quedan intentos y no se han descubierto todas las letras, se ejecutara
+    while((intentos > 0) && (palabraOculta !== palabraSecreta)){
+        // Hacemos un prompt
+        let letra = prompt("Dame una letra y adivina la palabra secreta:");
+
+        // Comprobamos si la letra esta en la palabra para actualizar la palabraOculta. El método .includes busca dentro de la palabraSecreta la letra que le pasamos
+        if(palabraSecreta.includes(letra)){
+            // Actualizamos la variable palabraOculta y metemos en la posicion correcta la letra
+            // Recorremos todas las letras de la palabra secreta
+            for(let contadorLetras = 0; contadorLetras < palabraSecreta.length; contadorLetras++){
+
+                // Como palabraOculta y palabraSecreta tienen la misma longitud, solo tenemos que buscar el indice de la letra que hemos acertado 
+                if(letra === palabraSecreta[contadorLetras]){
+                    // Si la letra es igual al indice que le pasamos, convertimos la palabra en un array para actualizar ese índice
+                    let palabraOcultaArray = Array.from(palabraOculta)
+
+                    // Seleccionamos la posicion del indice y cambiamos el guion bajo "_" por la letra que hemos introducido en el prompt
+                    palabraOcultaArray[contadorLetras] = letra;
+
+                    // Unimos el array en un nuevo string sin ningun tipo de separaciones y actualizamos la palabra habiendo descubierto una de las letras
+                    palabraOculta = palabraOcultaArray.join("");
+                }
+            }// End for
+        }else{
+            // Si no has acertado resta un intento
+            intentos --;
+        }
+        // Mostramos el estado actual de la palabra oculta
+        console.log(`La palabra es: ${palabraOculta}`);
+
+        // Mostrar cuantos intentos quedan
+        console.log(`Te queda un total de ${intentos} intentos.`);
+
+    }// End While
+
+    //Mostrar msj ed victoria o de derrota
+    if(palabraOculta == palabraSecreta){
+        console.log(`Felicidades, has ganado la partida, la palabra era: ${palabraSecreta}`);
+    } else {
+        console.log(`Lo siento, has perdido la partida, otra vez será. La palabra era: ${palabraSecreta}`)
+    }
 }
 
 const lanzarJuego = document.querySelector("#lanzarJuego");
 
-lanzarJuego.addEventListener("click", () => juegoDelAhorcado("pekora"));
+lanzarJuego.addEventListener("click", () => juegoDelAhorcado("pekoraeslamejor"));
 
 
-let palabraConstruida ="";
-let intentos = 5;
-let palabra = [];
-var letra = prompt("Di una letra: " + " tienes " + intentos + " aún");
-const palabraSecreta = "pekora";
-
-// Recorremos la palabra secreta
-for (let i = 0; i < palabraSecreta.length; i++) {
-    // Almacenamos las letras de la palabra secreta en el array palabra
-    palabra.push(palabraSecreta[i]);
-    if (palabra){
-        // Pintamos un guion cuando se encuentre algo dentro del array
-        palabraConstruida += "_ ";
-    }
- 
-}
-
-console.log(letra);
-console.log(palabra);
-console.log(palabraConstruida);
-
-var pintar = document.querySelector("#palabra");
-pintar.innerHTML = palabraConstruida;
 
