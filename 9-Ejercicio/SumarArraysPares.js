55f883871cb6139a5c1cfd146cdf844b95752b0f
CarlosGarciaDMarina <117413251+CarlosGarciaDMarina@users.noreply.github.com>
2024-04-27 23:38:05 +0200

    Ejercicio numero 9

--- 9-Ejercicio/SumarArraysPares.js
+++ 9-Ejercicio/SumarArraysPares.js
@@ -15,7 +15,24 @@ Ejemplos:
 sumarPares([2, 13, 6, 15]);  // Resultado: 8
  
 */
+var array = [2, 13, 6, 15];
 
+// Funcion que suma los numeros pares que haya dentro de un array 
 function sumarPares(array) {
-    //
+    // Definimos las variables que vamos a necesitar
+    var resultado = 0;
+
+    // creamos un bucle que lea el array al completo
+    for (let i = 0; i < array.length; i++) {
+
+        // Creamos un condicional que, cuando un elemento del array sea par, se sume a la variable resultado
+        if((array[i] % 2) == 0){
+            resultado += array[i];
+        }
+    }
+    // Devolvemos el resultado final
+    return resultado;   
 }
+
+// Mostramos el resultado
+console.log(`Resultado: ${sumarPares(array)}`);
